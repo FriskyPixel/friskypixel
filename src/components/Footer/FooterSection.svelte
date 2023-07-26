f58ba6c160a5010b1920724b03d422bd97e2bc93
David Frisk <davidfrisk82@gmail.com>
2023-07-26 12:58:25 -0400

    add copyright to footer

--- src/components/Footer/FooterSection.svelte
+++ src/components/Footer/FooterSection.svelte
@@ -4,29 +4,36 @@
   import GithubLogo from '$lib/images/icons/github.svelte';
 
   const iconWidth = 50;
+  const date = new Date();
 </script>
 
 <footer>
-  <a href="https://www.linkedin.com/in/friskypixel/" target="_blank">
-    <LinkedinLogo width={iconWidth} />
-  </a>
-  <a href="https://www.youtube.com/c/DavidFrisk/" target="_blank">
-    <YoutubeLogo width={iconWidth} />
-  </a>
-  <a href="https://github.com/FriskyPixel" target="_blank">
-    <GithubLogo width={iconWidth} />
-  </a>
+  <div class="links">
+    <a href="https://www.linkedin.com/in/friskypixel/" target="_blank">
+      <LinkedinLogo width={iconWidth} />
+    </a>
+    <a href="https://www.youtube.com/c/DavidFrisk/" target="_blank">
+      <YoutubeLogo width={iconWidth} />
+    </a>
+    <a href="https://github.com/FriskyPixel" target="_blank">
+      <GithubLogo width={iconWidth} />
+    </a>
+  </div>
+  <div class="copyright">&copy; {date.getFullYear()}</div>
 </footer>
 
 <style lang="scss">
   footer {
+    background: var(--c-black);
+    padding: 4em 4em 2em;
+  }
+
+  .links {
     --icon-ci: white;
 
     display: flex;
     justify-content: center;
     gap: 3em;
-    background: var(--c-black);
-    padding: 4em;
   }
 
   a {
@@ -37,5 +44,11 @@
       --icon-ci: var(--c-red);
       transform: scale(1.1);
     }
+  }
+
+  .copyright {
+    color: white;
+    text-align: center;
+    margin-top: 1.5em;
   }
 </style>
